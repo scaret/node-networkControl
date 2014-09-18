e0c1831f17ca29c29f6536e1896c759fdc9b6838
Derek Lu <yuchun.lu@mesheven.com>
2014-09-18 11:46:52 +0800

    Add execRoutePrint4

--- networkUtil/win32.js
+++ networkUtil/win32.js
@@ -114,6 +114,62 @@ var execNetshMbnShowInterfaces = function(callback)
 	});
 };
 
+var execRoutePrint4 = function (callback)
+{
+	execDone('route PRINT -4', function (stdoutLines){
+		var info = {interfaceList: [], activeRoutes:[]};
+		var interfaceListFlag = false;
+		var activeRouteFlag   = false;
+		var routeKeys;
+		for (var i = 0; i < stdoutLines.length; i++){
+			var line = stdoutLines[i];
+			if (line == 'Interface List'){
+				interfaceListFlag = true;
+				continue;
+			}
+			if (line.match(/Network Destination/))
+			{
+				routeKeys = line.trim().split(/ {2,30}/);
+
+				activeRouteFlag = true;
+				continue;
+			}
+			if (line.match(/={20,40}/g))
+			{
+				var interfaceListFlag = false;
+				var activeRouteFlag   = false;
+				continue;
+			}
+			if (interfaceListFlag)
+			{
+				var match = line.match(/(\d{1,3})[\. ]*([\da-f ]{17})?[\. ]*([^\n]+)/);
+				if (match)
+				{
+					var interface = {
+						"if": match[1],
+						macAddress: match[2],
+						deviceName : match[3]
+					};
+					info.interfaceList.push(interface);
+					console.log(interface);
+				}
+			}
+			if (activeRouteFlag)
+			{
+				var routeValues = line.trim().split(/ {2,30}/);
+				var routeItem = {};
+				for(var j in routeValues)
+				{
+					routeItem[routeKeys[j]] = routeValues[j];
+				}
+				info.activeRoutes.push(routeItem);
+			}
+		}
+		callback(info);
+	});
+};
+
 module.exports.execIpconfigAll = execIpconfigAll;
 module.exports.execNetshInterfaceIpv4ShowInterfaces = execNetshInterfaceIpv4ShowInterfaces;
 module.exports.execNetshMbnShowInterfaces = execNetshMbnShowInterfaces;
+module.exports.execRoutePrint4 = execRoutePrint4;
